89810844a881f7ca26cfae6aadb4efda9c408345
thanhduong0902 <96873431+thanhduong0902@users.noreply.github.com>
2022-04-01 22:55:23 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,50 +3,49 @@ import "./App.css";
 import Expenselist from "./components/Expenselist";
 import React from "react";
 import Additem from "./components/AddItem";
-import Filterexpense from "./components/Filterexpense";
+import { v4 as uuidv4 } from "uuid";
 function App() {
   const [items, setItems] = useState([
     {
+      id: uuidv4(),
       text: "Some book",
       date: "2022-03-26",
       price: "$ 50",
     },
     {
+      id: uuidv4(),
       text: "Electric Bill",
       date: "2022-03-26",
       price: "$ 70",
     },
     {
+      id: uuidv4(),
       text: "House",
       date: "2022-03-27",
-      price: "$1000",
+      price: "$ 1000",
     },
   ]);
   const additem = (value, amount, date) => {
-    setItems([...items, { text: value, date: date, price: amount }]);
+    setItems([
+      ...items,
+      { text: value, date: date, price: amount, id: uuidv4() },
+    ]);
   };
-  // const filter = (year) => {
-  //   const yearft = items.filter((item) => {
-  //     if (year === new Date(item.date).getFullYear()) {
-  //       return true;
-  //     }
-  //     return false;
-  //   });
-  // };
+  const deleteItem = (id) => {
+    const updatedItems = items.filter((item) => item.id !== id);
+    setItems(updatedItems);
+  };
+  const filter = (year) => {
+    const updatedItems = items.filter(
+      (item) => new Date(item.date).getFullYear() === Number(year)
+    );
+    setItems(updatedItems);
+  };
 
-  const addyear = (year) => {
-    year = Number(year);
-    let array = items.filter((item) => {
-      if (year === new Date(item.date).getFullYear()) return true;
-      return false;
-    });
-    console.log(array);
-  };
   return (
     <div className="App">
       <Additem additem={additem} />
-      <Filterexpense filter={addyear} />
-      <Expenselist items={items} />
+      <Expenselist items={items} deleteItem={deleteItem} filter={filter} />
     </div>
   );
 }
