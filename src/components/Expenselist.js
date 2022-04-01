cb7ceef5d4e82ed33f34599e24461113dd661c56
thanhduong0902 <96873431+thanhduong0902@users.noreply.github.com>
2022-04-01 22:53:34 +0700

    Update Expenselist.js

--- src/components/Expenselist.js
+++ src/components/Expenselist.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 
 function Expenselist(props) {
+  const [year, setYear] = useState("");
+  const getYear = (e) => {
+    setYear(e.target.value);
+  };
+
   return (
-    <div className="list-expense">
-      {props.items.map((item, index) => (
-        <ExpenseItem
-          key={index}
-          text={item.text}
-          price={item.price}
-          date={item.date}
-        />
-      ))}
+    <div className="list">
+      <label className="filter">
+        Filter by year
+        <select className="year" onChange={getYear}>
+          <option>2020</option>
+          <option>2021</option>
+          <option>2022</option>
+        </select>
+        <button onClick={() => props.filter(year)}>Lọc</button>
+      </label>
+      <div className="list-expense">
+        {props.items.map((item) => (
+          <ExpenseItem
+            key={item.id}
+            id={item.id}
+            text={item.text}
+            price={item.price}
+            date={item.date}
+            deleteItem={props.deleteItem}
+          />
+        ))}
+      </div>
     </div>
   );
 }
